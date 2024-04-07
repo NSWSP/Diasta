31096c4ee6aa162fd3e381c0b59836fa341af2c9
NSWSP <naanani.mohammed.10@gmail.com>
2024-04-07 14:56:43 +0200

    Premier Aller Retour

--- app/src/main/java/com/example/diasta/accueil.java
+++ app/src/main/java/com/example/diasta/accueil.java
@@ -2,13 +2,26 @@ package com.example.diasta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class accueil extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
+        Button btnRetourversMain = findViewById(R.id.btnRetourversMain);
+
+        // Définissez un écouteur de clics sur le bouton
+        btnRetourversMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Créez une Intent pour démarrer l'activité Accueil
+                Intent intent = new Intent(accueil.this, MainActivity.class);
+                startActivity(intent); // Démarrez l'activité
+            }
+        });
     }
 }
